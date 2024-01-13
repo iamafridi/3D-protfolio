8032e94b993d7cf1b2e69e43eeb52652b74d2d1d
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-13 17:15:13 +0600

    Added nbv bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Home, About, Projects, Contact } from './pages';
 
 const App = () => {
     return (
         <main className='bg-slate-300/20'>
-<Router>
-    <Navbar />
-    <Routes>
-        <Route path='/' element={'Home'} />
-        <Route path='/about' element={'Know More About Me'} />
-        <Route path='/projects' element={'Projects'} />
-        <Route path='/contact' element={'Contact Me'} />
-    </Routes>
-</Router>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/projects' element={<Projects />} />
+                    <Route path='/contact' element={<Contact />} />
+                </Routes>
+            </Router>
         </main>
     );
 };
