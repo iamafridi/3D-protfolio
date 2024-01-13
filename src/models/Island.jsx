ca6c8edc014590b8cb7c4a2273aa8440e525cf93
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-14 00:23:33 +0600

    animated the bird to move across using useFrame and calculations

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -16,7 +16,7 @@ import { a } from "@react-spring/three";
 
 import islandScene from '../assets/3d/destiny_island.glb'
 
-const Island = ({ isRotating, setIsRotating, ...props }) => {
+const Island = ({ isRotating, setIsRotating,setCurrentStage, ...props }) => {
     const islandRef = useRef();
 
     const { gl, viewport } = useThree();
@@ -86,6 +86,7 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
             if (Math.abs(rotationSpeed.current) < 0.001) {
                 rotationSpeed.current = 0
             }
+            islandRef.current.rotation.y += rotationSpeed.current
         }
         else {
             const rotation = islandRef.current.rotation.y;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,10 @@ import Plane from '../models/Plane'
 
 const Home = () => {
 
-const [isRotating , setIsRotating] = useState(false);
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1)
 
-
-//  **For ISLAND ***
+  //  **For ISLAND ***
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -31,16 +31,16 @@ const [isRotating , setIsRotating] = useState(false);
 
   // ** for Plane ***
   const adjustPlaneForScreenSize = () => {
-    let screenScale,screenPosition ;
+    let screenScale, screenPosition;
 
     if (window.innerWidth < 768) {
       screenScale = [1.5, 1.5, 1.5];
-      screenPosition=[0, -1.5, 0]
+      screenPosition = [0, -1.5, 0]
 
     }
     else {
       screenScale = [3, 3, 3];
-      screenPosition=[0, -4, -4]
+      screenPosition = [0, -4, -4]
 
     }
     return [screenScale, screenPosition]
@@ -69,7 +69,7 @@ const [isRotating , setIsRotating] = useState(false);
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
 
           <Island
             position={islandPosition}
@@ -77,12 +77,13 @@ const [isRotating , setIsRotating] = useState(false);
             rotation={islandRotation}
             isRotating={isRotating}
             setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
           <Plane
-          isRotating={isRotating}
-          planeScale={planeScale}
-          planePosition={planePosition}
-          rotation={[0,20,0]}
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
           />
         </Suspense>
 
