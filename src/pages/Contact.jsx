b0cc0fba1cd0a552c4127850c656caf86350a80d
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-01-14 14:44:44 +0600

    added the fox

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,24 @@
-import React, { useRef, useState } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import { Form } from 'react-router-dom'
 import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+
+import Fox from '../models/Fox';
+import Loader from '../components/Loader';
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' })
   const [isLoading, setIsLoading] = useState(false)
+  const [currentAnimation, setCurrentAnimation] = useState('idle')
 
   const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value})
-   };
+    setForm({ ...form, [e.target.name]: e.target.value })
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true)
+    setCurrentAnimation('hit')
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -24,20 +30,24 @@ const Contact = () => {
         to_email: 'contact@afridiakabarifty',
         message: form.message
       },
-      
-      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
-    ).then (()=>{
-      setIsLoading(false);
 
-      setForm({ name: '', email: '', message: '' })
-    }).catch((error)=>{
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+    ).then(() => {
+      setIsLoading(false);
+      setTimeout(() => {
+        setCurrentAnimation('idle')
+        setForm({ name: '', email: '', message: '' })
+      }, [3000])
+
+    }).catch((error) => {
       setIsLoading(false)
+      setCurrentAnimation('idle')
       console.log(error);
     })
-   };
+  };
 
-  const handleFocus = () => { };
-  const handleBlur = () => { };
+  const handleFocus = () => { setCurrentAnimation('Walk') };
+  const handleBlur = () => { setCurrentAnimation('idle') };
 
 
   return (
@@ -105,7 +115,28 @@ const Contact = () => {
           </button>
         </form>
       </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
+          }}
+        >
 
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />} >
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
   )
 }
